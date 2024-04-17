9c0db965e7e2894a7d98526d75adc1796ea3541e
datle-dev <dle.dev@outlook.com>
2024-04-17 11:36:32 -0400

    Add routes

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+const port = 8080;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/about.html");
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile(__dirname + "/contact-me.html");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).sendFile(__dirname + "/404.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
